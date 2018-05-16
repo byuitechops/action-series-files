61ebb07066ee23fac48081fee0da0179219d90dc
Zachary Williams <wil11063@byui.edu>
2018-05-16 09:55:02 -0600

    Add pub filetype, fix course schedule regex

--- actions/files-delete.js
+++ actions/files-delete.js
@@ -23,7 +23,7 @@ module.exports = (course, file, callback) => {
             /discussion\sforums/gi,
             /setup\s*notes\s*for\s*development\s*team/gi,
             /how\s*to\s*understand\s*due\s*date(s)*/gi,
-            /course\s*schedule\d*\D*archived/gi,
+            /course\s*schedule/gi,
             new RegExp(`${course.info.courseName}banner`, 'i'),
             new RegExp(`${course.info.courseName}thumbnail`, 'i'),
         ];
